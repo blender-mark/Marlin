0b19624b169b74940a1f94b7448dbd0c53a16f5a
blender-mark <indekeu.marc@gmail.com>
2020-12-09 07:59:27 +0100

    Update Configuration.h
    Update bed size v-king

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1120,7 +1120,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 380
-#define Y_BED_SIZE 380
+#define Y_BED_SIZE 340
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1128,7 +1128,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 350
+#define Z_MAX_POS 330
 
 /**
  * Software Endstops
